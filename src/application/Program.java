6a80686da54dd94bbef706ea10097bfe036b0f6b
Thiago-AM-Souza <thiagosouza96.ts@gmail.com>
2022-02-17 16:20:41 -0300

    findByDepartment implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,6 +7,7 @@ import model.entities.Department;
 import model.entities.Seller;
 
 import java.util.Date;
+import java.util.List;
 
 public class Program {
     public static void main(String[] args) {
@@ -16,7 +17,12 @@ public class Program {
         System.out.println("=== Test 1: seller by id ===");
         Seller seller = sellerDao.findById(2);
 
-        System.out.println(seller);
+        System.out.println("\n=== Test 1: seller by id ===");
+        Department department = new Department(2, null);
+        List<Seller> list = sellerDao.findByDepartment(department);
+        for (Seller obj: list){
+            System.out.println(obj);
+        }
 
     }
 }
